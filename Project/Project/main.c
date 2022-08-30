0c5def276baa09cd593ea7aeb66823e361372f4a
Sergiu Maier <67898827+SergiuMaier@users.noreply.github.com>
2022-08-30 20:29:38 +0300

    Update main.c

--- Project/Project/main.c
+++ Project/Project/main.c
@@ -6,9 +6,9 @@
  */ 
 
 #define F_CPU 16000000UL
-#define TIMP_ON 7					//durata de timp in secunde cat LED-urile sunt aprinse
+#define TIMP_ON 7		    //durata de timp in secunde cat LED-urile sunt aprinse
 #define GENERARE_INTRERUPERE 0.001  //generarea unei intreruperi la fiecare x secunde
-#define VALOARE_TOP_REGISTRU 256	//valoarea de top a registrului TCNT0 (pt ca folosesc timer pe 8 biti)
+#define VALOARE_TOP_REGISTRU 256    //valoarea de top a registrului TCNT0 (pt ca folosesc timer pe 8 biti)
 
 //prescalari
 #define TIMER_NO_PRESCALAR     ((0 << CS02) | (0 << CS01) | (1 << CS00))        // 001
@@ -26,9 +26,9 @@ void pinReset(volatile uint8_t *port, uint8_t pin);
 void ledOn();
 int calculValoareRegistru();
 
-int flag_on = 0;			//initial, led-urile sunt stinse
+int flag_on = 0;		//initial, led-urile sunt stinse
 int contor_secunde = 0;		//contor pentru generarea secundelor
-int contor_on = 0;			//contor pentru masurarea duratei de timp in care LED-urile sunt aprinse
+int contor_on = 0;		//contor pentru masurarea duratei de timp in care LED-urile sunt aprinse
 int secunde = 0;			
 volatile uint8_t prescalar;
 
@@ -50,7 +50,7 @@ int main(void){
 	//momentan time-ul executa o intrerupere la fiecare 1ms
 	
 	TCCR0A |= (1 << WGM01) | (0 << WGM00);  //CTC		
-	TCCR0B |= TIMER_PRESCALAR_64;			//valoare prescalar
+	TCCR0B |= TIMER_PRESCALAR_64;		//valoare prescalar
 	OCR0A = calculValoareRegistru();	
 	TIMSK0 |= (1 << OCIE0A);
 					      
@@ -75,7 +75,7 @@ void pinReset(volatile uint8_t *port, uint8_t pin){
 
 void ledOn(){
 	
-	if(secunde % 2 == 0){
+	if(secunde % 2 == 0){		//test timer
 		pinSet(&PORTB, PINB0);
 	}
 	
@@ -141,7 +141,7 @@ ISR(INT0_vect){ //pt butonul de on
 	cli();
 	
 	flag_on = 1;		 //LED-uri on
-	contor_on = secunde; //determinarea timpului la care a fost apasat butonul  
+	contor_on = secunde;     //determinarea timpului la care a fost apasat butonul  
 	
 	sei();
 }
@@ -150,7 +150,7 @@ ISR(INT1_vect){ //pt butonul de off
 	
 	cli();
 	
-	flag_on = 0; //LED-uri off
+	flag_on = 0;  //LED-uri off
 	
 	sei();
 }
