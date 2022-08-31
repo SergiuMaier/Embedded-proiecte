6ff4c344b74c8ffe5cbbced25b577449117e78f4
Sergiu Maier <67898827+SergiuMaier@users.noreply.github.com>
2022-08-31 15:06:37 +0300

    Update main_func.c

--- 1_timer0_general/Project/main_func.c
+++ 1_timer0_general/Project/main_func.c
@@ -9,9 +9,9 @@
 #include "main_defines.h"
 #include <stdio.h>
 
-int flag_on = 0;			//initial, led-urile sunt stinse
+int flag_on = 0;		//initial, led-urile sunt stinse
 int contor_secunde = 0;		//contor pentru generarea secundelor
-int contor_on = 0;			//contor pentru masurarea duratei de timp in care LED-urile sunt aprinse
+int contor_on = 0;		//contor pentru masurarea duratei de timp in care LED-urile sunt aprinse
 int secunde = 0;
 
 void pinSet(volatile uint8_t *port, uint8_t pin){
@@ -26,10 +26,10 @@ void pinReset(volatile uint8_t *port, uint8_t pin){
 
 void ledOn(){
 	
-	if(secunde % 2 == 0)				  //testare timer
+	if(secunde % 2 == 0)		//testare timer
 		pinSet(&PORTB, PINB0);
 	
-	if(flag_on == 1){					  //aprindere LED-uri la apasarea butonului
+	if(flag_on == 1){		//aprindere LED-uri la apasarea butonului
 		pinSet(&PORTB, PINB0);
 		pinSet(&PORTB, PINB1);
 		pinSet(&PORTB, PINB2);
@@ -70,7 +70,7 @@ void calcul_valoare_registru_ocr0a(){
 		//prescalar = i;
 	}
 	
-/*-----aici s-a incercat modificarea automat? a presalarului în registrul TCCR0B---------/
+/*-----aici s-a incercat modificarea automat? a presalarului Ã®n registrul TCCR0B---------/
 //s-au testat mai multe modalitati de parcurgere a valorilor, toate nefunctionale. 
 		
 	if(prescalar == 0)
@@ -106,11 +106,11 @@ ISR(TIMER0_COMPA_vect){  //pt caz general
 	sei();
 }
 
-ISR(INT0_vect){			//intrerupere pt butonul de on
+ISR(INT0_vect){		     //intrerupere pt butonul de on
 	
 	cli();
 	
-	flag_on = 1;		 //LED-uri on
+	flag_on = 1;	     //LED-uri on
 	contor_on = secunde; //determinarea timpului la care a fost apasat butonul
 	
 	sei();
