377dbe3a8bbeaba849fb962e43f35821ed447a2e
Sergiu Maier <sergiumihaimaier@yahoo.com>
2022-10-05 16:57:27 +0300

    ren 07

---
+++ 07_spi_rx_tx_complete/SPI/USART.h
@@ -0,0 +1,30 @@
+/*
+ * main_func.h
+ *
+ * Created: 9/29/2022 11:46:43 AM
+ *  Author: Sergiu Maier
+ */ 
+
+#ifndef MAIN_FUNC_H_
+#define MAIN_FUNC_H_
+
+#include "main_defines.h"
+
+#include <util/delay.h>
+
+volatile uint8_t flag_tx;
+volatile uint8_t flag_rx;
+volatile uint8_t afiseaza_timp;
+uint8_t flag_afisare_timp;
+
+void init_USART(uint16_t ubrr);
+
+void send_data(char *c);
+
+void receive_data();
+
+ISR(USART_UDRE_vect);
+
+ISR(USART_RX_vect);
+
+#endif /* MAIN_FUNC_H_ */
