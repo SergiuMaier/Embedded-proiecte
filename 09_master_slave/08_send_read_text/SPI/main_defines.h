932e06a4fda75a6c4a6c456b331a85216cfeac2a
Sergiu Maier <sergiumihaimaier@yahoo.com>
2022-10-14 09:47:45 +0300

    crearea folder

---
+++ 09_master_slave/08_send_read_text/SPI/main_defines.h
@@ -0,0 +1,20 @@
+/*
+ * main_defines.h
+ *
+ * Created: 9/21/2022 11:46:26 AM
+ *  Author: sergiu
+ */ 
+
+#ifndef MAIN_DEFINES_H_
+#define MAIN_DEFINES_H_
+
+#include <stdlib.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#define CLEAR "\033\143"
+#define F_CPU 16000000UL
+#define BAUD 9600
+#define MYUBRR (F_CPU/16/BAUD)-1
+
+#endif /* MAIN_DEFINES_H_ */
