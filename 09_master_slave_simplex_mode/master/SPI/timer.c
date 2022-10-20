c7d463b5ef193a79e70eae5080c4203fdb158f04
Sergiu Maier <sergiumihaimaier@yahoo.com>
2022-10-20 12:49:48 +0300

    simplex mode

---
+++ 09_master_slave_simplex_mode/master/SPI/timer.c
@@ -0,0 +1,55 @@
+/*
+ * main_func.c
+ *
+ * Created: 9/26/2022 11:12:11 AM
+ *  Author: Sergiu Maier
+ */ 
+
+#include "timer.h"
+
+void init_timer(){
+
+	TCCR0A |= (1 << WGM01)|(0 << WGM00);           //mod ctc
+	OCR0A = 0xF9;                                  //pt 10ms
+	TCCR0B |= (0 << CS02)|(1 << CS01)|(1 << CS00); //prescalar 64
+	TIMSK0 |= (1 << OCIE0A);
+}
+
+void init_devices(){
+	
+	cli();
+	
+	//init_porturi:
+	DDRB = 0x03;
+	PORTB = 0x00;
+	
+	init_timer();
+	
+	sei();
+}
+
+ISR(TIMER0_COMPA_vect){
+	
+	cli();
+	
+	contor++;
+	contor_led_on++;
+	
+	if(contor >= 1000){
+		secunde++;
+		contor = 0;
+	}
+	
+	sei();
+}
+
+void incrementare_minute(){
+	
+	if(secunde >= 60){	//in ISR aveam 01:59 in loc de 01:00
+		secunde %= 60;
+		minute++;
+		
+	}
+}
+
+
