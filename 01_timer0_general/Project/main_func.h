9553a6218e6363ab3f175e39c88c841f075fa656
Sergiu Maier <sergiumihaimaier@yahoo.com>
2022-10-24 14:47:32 +0300

    rm makefile

--- 01_timer0_general/Project/main_func.h
+++ 01_timer0_general/Project/main_func.h
@@ -15,17 +15,11 @@
 #include <util/delay.h>
 
 void pinSet(volatile uint8_t *port, uint8_t pin);
-
 void pinReset(volatile uint8_t *port, uint8_t pin);
-
 void ledOn();
-
 void calcul_valoare_registru_ocr0a();
-
 ISR(TIMER0_COMPA_vect);
-
 ISR(INT0_vect);
-
 ISR(INT1_vect);
 
 #endif /* MAIN_FUNC_H_ */
