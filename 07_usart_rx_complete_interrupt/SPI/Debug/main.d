6845065471434bfd7d5a8d502d901708e0a9e25e
Sergiu Maier <sergiumihaimaier@yahoo.com>
2022-10-24 11:19:34 +0300

    rename proj

---
+++ 07_usart_rx_complete_interrupt/SPI/Debug/main.d
@@ -0,0 +1,70 @@
+main.d main.o: .././main.c .././USART.h .././main_defines.h \
+ d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
+ d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
+ d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ D:\DRIVERE\7.0\Packs\atmel\ATmega_DFP\1.7.374\include/avr/iom328p.h \
+ d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
+ .././functii_afisare.h .././timer.h \
+ d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
+ d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h \
+ d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h \
+ d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
+
+.././USART.h:
+
+.././main_defines.h:
+
+d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
+
+d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
+
+d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+D:\DRIVERE\7.0\Packs\atmel\ATmega_DFP\1.7.374\include/avr/iom328p.h:
+
+d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
+
+.././functii_afisare.h:
+
+.././timer.h:
+
+d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h:
+
+d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h:
+
+d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h:
+
+d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+d:\drivere\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
---
+++ 07_usart_rx_complete_interrupt/SPI/main.c
@@ -0,0 +1,23 @@
+/*
+ * SPI.c
+ *
+ * Created: 9/29/2022 10:53:40 AM
+ * Author : Sergiu Maier
+ */ 
+
+#include "USART.h"
+
+int main(void)
+{	
+	init_devices();
+	init_USART(MYUBRR);
+	
+	send_data(CLEAR);
+	afisare_meniu();
+	new_line();
+	
+	while(1){
+	
+    }
+}
+
