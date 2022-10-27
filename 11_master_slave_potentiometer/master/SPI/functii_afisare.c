54b2885938514d5b6a63d8273b68cd435d99a2fd
Sergiu Maier <sergiumihaimaier@yahoo.com>
2022-10-27 13:51:31 +0300

    update slave

--- 11_master_slave_potentiometer/master/SPI/functii_afisare.c
+++ 11_master_slave_potentiometer/master/SPI/functii_afisare.c
@@ -19,11 +19,19 @@ void switch_data(char data[])
 		send_data("TIMP\n\r");
 	}
 	else if(strcmp(data, "4") == 0){
+		send_data("VALOARE\n\r");
+	}
+	else if(strcmp(data, "5") == 0){
 		send_data("CLEAR\n\r");
 	}
 	else{
+		
+		//asa afisez momentan raspunsul de la slave
+		
 		send_data("r: ");
 		send_data(data);
 		send_data("\n\r");
+		
+		//trebuie tratate si comenzile incorecte cumva!
 	}
 }
